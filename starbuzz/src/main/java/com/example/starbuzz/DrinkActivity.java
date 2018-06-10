1fcdd1a3b18906f4657964750feb066ee193be7d
DirecTion1024 <fx18358196136@gmail.com>
2018-06-10 22:48:26 +0800

    增加DrinkActivity

---
+++ starbuzz/src/main/java/com/example/starbuzz/DrinkActivity.java
@@ -0,0 +1,20 @@
+package com.example.starbuzz;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DrinkActivity extends AppCompatActivity {
+    public static final String EXTRA_DRINKNO = "drinkNo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink);
+
+    }
+
+}
