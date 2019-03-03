7c5b9e728e0d2aed00f3222893cd74a0e2509b74
direction1024 <fx18358196136@gmail.com>
2019-03-03 15:34:48 +0800

    fix activity_deatil.xml & EXTRA_WORKOUT_ID

--- workout/src/main/java/com/example/workout/DetailActivity.java
+++ workout/src/main/java/com/example/workout/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.workout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_WORKOUT_ID = "id";
@@ -13,7 +14,8 @@ public class DetailActivity extends AppCompatActivity {
         //得到片段的引用
         WorkoutDetailFragment workoutDetailFragment = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
         //从意图得到用户单击的训练项目的ID
-        int workoutId = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+        Log.d("workout", "EXTRA_WORKOUT_ID: "+getIntent().getExtras().get(EXTRA_WORKOUT_ID));
+        int workoutId = Integer.parseInt(getIntent().getExtras().get(EXTRA_WORKOUT_ID).toString()) ;
         //将训练项目ID传递给片段
         workoutDetailFragment.setWorkoutId(workoutId);
     }
